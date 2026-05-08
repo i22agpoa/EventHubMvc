447f353b171a4c9fdfb0b25168ab5658da3e0c90
i22agpoa <i22agpoa@uco.es>
2026-05-08 15:37:40 +0200

    Update Program.cs

--- EventHubMvc/Program.cs
+++ EventHubMvc/Program.cs
@@ -1,6 +1,8 @@
 using EventHubMvc.Data;
 using EventHubMvc.Models;
 using Microsoft.EntityFrameworkCore;
+using EventHubMvc.Repositories;
+using EventHubMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventService, EventService>();
 
 var app = builder.Build();
 
