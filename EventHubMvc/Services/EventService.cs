02b55ade0d66417a70e577384fccbe71d2fe7d56
i22agpoa <i22agpoa@uco.es>
2026-05-08 15:34:45 +0200

    Create IEventService and EventService

---
+++ EventHubMvc/Services/EventService.cs
@@ -0,0 +1,50 @@
+﻿using EventHubMvc.Models;
+using EventHubMvc.Repositories;
+
+namespace EventHubMvc.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventService(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<List<Event>> GetAllEventsAsync()
+        {
+            return await _eventRepository.GetAllAsync();
+        }
+
+        public async Task<Event?> GetEventByIdAsync(int id)
+        {
+            return await _eventRepository.GetByIdAsync(id);
+        }
+
+        public async Task<List<Event>> SearchEventsAsync(string? searchTerm)
+        {
+            return await _eventRepository.SearchAsync(searchTerm);
+        }
+
+        public async Task CreateEventAsync(Event eventItem)
+        {
+            await _eventRepository.AddAsync(eventItem);
+        }
+
+        public async Task UpdateEventAsync(Event eventItem)
+        {
+            await _eventRepository.UpdateAsync(eventItem);
+        }
+
+        public async Task DeleteEventAsync(int id)
+        {
+            await _eventRepository.DeleteAsync(id);
+        }
+
+        public async Task<bool> EventExistsAsync(int id)
+        {
+            return await _eventRepository.ExistsAsync(id);
+        }
+    }
+}
---
+++ EventHubMvc/Services/IEventService.cs
@@ -0,0 +1,15 @@
+﻿using EventHubMvc.Models;
+
+namespace EventHubMvc.Services
+{
+    public interface IEventService
+    {
+        Task<List<Event>> GetAllEventsAsync();
+        Task<Event?> GetEventByIdAsync(int id);
+        Task<List<Event>> SearchEventsAsync(string? searchTerm);
+        Task CreateEventAsync(Event eventItem);
+        Task UpdateEventAsync(Event eventItem);
+        Task DeleteEventAsync(int id);
+        Task<bool> EventExistsAsync(int id);
+    }
+}
