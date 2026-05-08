62e32964a697505597747d1cb75d6acff2c58c69
i22agpoa <i22agpoa@uco.es>
2026-05-08 15:29:08 +0200

    Create IEventRepository.cs

---
+++ EventHubMvc/Respositories/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using EventHubMvc.Models;
+
+namespace EventHubMvc.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<List<Event>> GetAllAsync();
+        Task<Event?> GetByIdAsync(int id);
+        Task<List<Event>> SearchAsync(string? searchTerm);
+        Task AddAsync(Event eventItem);
+        Task UpdateAsync(Event eventItem);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
